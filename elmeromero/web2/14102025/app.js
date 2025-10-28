f33353d0b6ab77cf26ef11a9cb9b2240757fff01
CuecapanQuetzal221 <bruno.cuecapan.enp5.221@gmail.com>
2025-10-27 18:54:44 -0600

    No seXD

---
+++ elmeromero/web2/14102025/app.js
@@ -0,0 +1,48 @@
+let inventario =[];
+function mostrarMenu(){
+    return parseInt(prompt(
+        `Opciones disponibles
+        1.- Agregar producto
+        2.- Mostrar todo los productos
+        3.-Buscar producto por nombre
+        4.-Salir
+        Elige una opcion: `
+    ));
+}
+
+function agregarProducto(){
+    let nombre = prompt("Ingresa el nombre del producto");
+    let cantidad = parseInt(prompt("Ingresa la cantidad del producto"));
+    let precio = parseInt(prompt("Ingresa el precio del producto"));
+
+    if (cantidad > 0 && precio > 0) {
+        let producto = {
+            nombre: nombre,
+            cantidad: cantidad,
+            precio: precio
+        };
+
+        inventario
+
+    } else {
+        
+    }
+
+}
+
+function mostrarProducto(){
+    if (inventario.length ==0) {
+        alert ("Inventario vacio")
+    } else {
+        let mensaje = "Producto del inventario \n";
+        for (i = 0; i < inventario.length; i++){
+            mensaje = `Producto:  ${(i+1)}\n` + 
+            `Nombre: ${(inventario[i].nombre)}\n` +
+            `Precio: ${(inventario[i].precio)}\n` +
+            `Cantidad: ${(invetario[i].cantidad)}\n`+
+            "-----------------------------------------"
+
+    
+    
+    }    }
+}
